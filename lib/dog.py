090b48bc14e9d510bc3654e2c0a89034b2f140ce
irene <irenejim9504@gmail.com>
2024-03-08 23:33:12 +0300

    completed the attributes lab

--- lib/dog.py
+++ lib/dog.py
@@ -1,15 +1,38 @@
-#!/usr/bin/env python3
-
-APPROVED_BREEDS = [
-    "Mastiff",
-    "Chihuahua",
-    "Corgi",
-    "Shar Pei",
-    "Beagle",
-    "French Bulldog",
-    "Pug",
-    "Pointer"
-]
-
 class Dog:
-    pass
+    approved_breeds = ["Mastiff", "Chihuahua", "Corgi", "Shar Pei", "Beagle", "French Bulldog", "Pug", "Pointer"]
+
+   
+    def __init__(self, name="", breed=""):
+        self._name = name
+        self.breed = breed
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value
+        else:
+            print("Name must be a string between 1 and 25 characters.")
+
+
+    @property
+    def breed(self):
+        return self._breed
+
+    @breed.setter
+    def breed(self, value):
+        if value in self.approved_breeds:
+            self._breed = value
+        else:
+            print("Breed must be in list of approved breeds.")
+
+
+
+# Creating an instance of Dog
+my_dog = Dog("A", "Labrador")  # This will trigger the breed validation automatically
+print(my_dog.name)
+my_dog = Dog("Buddy", "Mastiff")
+print(my_dog.name)
